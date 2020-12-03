9a74ffb98762a46cae03b8f9776c0ab05265e556
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-03 14:22:56 +0530

    song data base

--- songsData/models.py
+++ songsData/models.py
@@ -1,5 +1,7 @@
+from uuid import uuid4
 from django.db import models
 from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
@@ -7,10 +9,36 @@ from django.db.models.base import Model
 class Genere(models.Model):
     id = models.UUIDField(primary_key=True, editable=False)
     genere = models.CharField(
-        unique=True, max_length=250, null=True, blank=True)
+        max_length=250, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         super(Genere, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.Genere.genere
+        return self.genere
+
+
+class Artist(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    artist = models.CharField(max_length=250, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        super(Artist, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.artist
+
+
+class Song(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    title = models.CharField(null=True, blank=True, max_length=250)
+    genere = models.ForeignKey(Genere, on_delete=CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=CASCADE)
+    image = models.URLField(blank=True, null=True, max_length=255)
+    link = models.URLField(blank=True, null=True, max_length=255)
+
+    def save(self, *args, **kwargs):
+        super(Song, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
