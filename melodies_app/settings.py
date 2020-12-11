fad0ed51757f3b6884fea82c36958b10f48a4258
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-11 22:18:09 +0530

    D1

--- melodies_app/settings.py
+++ melodies_app/settings.py
@@ -100,15 +100,15 @@ WSGI_APPLICATION = 'melodies_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
+WHITENOISE_USE_FINDERS = True
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
